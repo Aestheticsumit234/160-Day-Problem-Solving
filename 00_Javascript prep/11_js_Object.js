bb521b8a005cd272e9fe679ced997ee64544c2e8
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2026-01-03 01:36:48 +0530

    learn more about promise

--- 00_Javascript prep/11_js_Object.js
+++ 00_Javascript prep/11_js_Object.js
@@ -1,6 +1,62 @@
-const fs = require("fs");
-console.log("Game Start");
+// const { rejects } = require("assert");
+// const fs = require("fs");
+// const fsv2 = require("fs/promises");
+// const { resolve } = require("path");
 
+// console.log("Game Start");
+
+// --------------------------- Using Promise -------------------------------
+// const fetech = fsv2
+//   .readFile("Hello.txt", "utf-8")
+//   .then((content) => fsv2.writeFile("Demo.txt", content))
+//   .then(() => fsv2.unlink("Hello.txt"))
+//   .catch((err) => console.log(err));
+
+//
+//
+// --------------------------- Custome Promise -------------------------------
+// function readFileWithPromise(filePath, endcoding) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(filePath, endcoding, function (err, content) {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(content);
+//       }
+//     });
+//   });
+// }
+
+// function writeFileWithPromise(filePath, content) {
+//   return new Promise((resolve, reject) => {
+//     fs.writeFile(filePath, content, function (err, data) {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// }
+
+// function unlinkFileWithPromise(filePath) {
+//   return new Promise((resolve, reject) => {
+//     fs.unlink(filePath, function (err) {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve("File Deleted");
+//       }
+//     });
+//   });
+// }
+
+// const fetechWithCustomPromise = readFileWithPromise("Hello.txt", "utf-8")
+//   .then((content) => writeFileWithPromise("Demo.txt", content))
+//   .then(() => unlinkFileWithPromise("Hello.txt"))
+//   .catch((err) => console.log(err));
+
+// --------------------------- Using Callback -------------------------------
 fs.readFile("Hello.txt", "utf-8", function (err, data) {
   if (err) {
     console.log(err);
@@ -22,3 +78,27 @@ fs.readFile("Hello.txt", "utf-8", function (err, data) {
     });
   }
 });
+
+// custom Promise
+
+function getData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          reject(res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+console.log("Promise Ending....");
+getData("https://jsonplaceholder.typicode.com/todos")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
